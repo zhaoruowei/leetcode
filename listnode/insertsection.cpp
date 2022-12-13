65685663e7fe1fb2cba9c2bf74194dc19d1d5c8f
zhaoruowei <38174230+zhaoruowei@users.noreply.github.com>
2022-12-13 17:22:34 +0800

    Create insertsection.cpp

---
+++ listnode/insertsection.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/* 
+判断两链表是否相交, 并返回相交结点 
+*/
+
+class ListNode
+{
+public:
+	int val;
+	ListNode* next;
+public:
+	ListNode(int val)
+	{
+		this->val = val;
+		this->next = nullptr;
+	}
+};
+
+class sloution
+{
+public:
+	
+};
