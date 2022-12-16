d4f7daf8402b8de30b90527bc156fefa4cab4f23
zhaoruowei <38174230+zhaoruowei@users.noreply.github.com>
2022-12-16 13:58:42 +0800

    Update traversal.cpp

--- tree/traversal.cpp
+++ tree/traversal.cpp
@@ -139,4 +139,53 @@ public:
 	        if (cur->right) list.emplace(cur->right);
 	    }
 	}
+	// 层序遍历分层输出
+	vector<vector<int>> bfsEachFloor(TreeNode* root)
+	{
+	    // 存储所有层的结果
+	    vector<vector<int>> ans;
+	    // 存储每一层的结果
+	    vector<int> res;
+	    if (!root)
+	    {
+	        return ans;
+	    }
+	    // 辅助队列，用于层序遍历
+	    queue<TreeNode*> list;
+	    list.emplace(root);
+	    // 记录当前层最后一个结点和下一层最后一个结点
+	    TreeNode* curend = root; // 初始为root
+	    TreeNode* nextend = nullptr; // 初始为空
+	    // 遍历二叉树
+	    while (!list.empty())
+	    {
+	        TreeNode* cur = list.front();
+	        list.pop();
+	        // 左右孩子入队，并更新nextend值
+	        if (cur->left)
+	        {
+	            list.emplace(cur->left);
+	            nextend = cur->left;
+	        }
+	        if (cur->right)
+	        {
+	            list.emplace(cur->right);
+	            nextend = cur->right;
+	        }
+	        // 处理当前结点的值，判断当前结点是否是当前层最后一个节点
+	        if (cur != curend) // 不时，在当前层数组中记录当前节点值
+	        {
+	            res.emplace_back(cur->val);
+	        }
+	        else // 是当前层最后一个结点，进行结算
+	        {
+                curend = nextend; // 重置当前层最后一个结点为下一层最后一个结点
+                nextend = nullptr; // 重置下一层最后一个节点
+                res.emplace_back(cur->val); // 记录最后一个节点的值
+                ans.emplace_back(res); // 存储当前层结果
+                res.clear(); // 重置每层存储数组
+	        }
+	    }
+	    return ans;
+	}
 };
