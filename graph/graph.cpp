dc3827eea8d154efcbd9fe64ae58798756030d1e
zhaoruowei <38174230+zhaoruowei@users.noreply.github.com>
2022-12-18 23:03:42 +0800

    Update graph.cpp

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -13,16 +15,6 @@ using namespace std;
 class Node;
 class Edge;
 class Graph;
-
-// 图结构
-class Graph
-{
-public:
-    unordered_map<int, Node*> nodes; // 图中的所有点集，int为编号，Node*为点的指针
-    unordered_set<Edge*> edges; // 图中的所有边集合，Edge*为边的指针
-    Graph()
-    {}
-};
 
 // 点结构
 class Node
@@ -55,6 +47,227 @@ public:
         this->to = to;
     }
 };
+
+// 图结构
+class Graph
+{
+public:
+    unordered_map<int, Node*> nodes; // 图中的所有点集，int为编号，Node*为点的指针
+    unordered_set<Edge*> edges; // 图中的所有边集合，Edge*为边的指针
+    Graph()
+    {}
+    ~Graph()
+    {
+        // 删除所有点
+        for (auto [_1, node] : nodes)
+        {
+            if (node) delete node;
+        }
+        // 删除所有边
+        for (auto edge : edges)
+        {
+            if (edge) delete edge;
+        }
+    }
+public:
+    // 宽度优先遍历
+    void BFS(Node* node)
+    {
+        if (!node)
+        {
+            return;
+        }
+        
+        queue<Node*> list;
+        unordered_set<Node*> visited;
+        list.emplace(node);
+        visited.emplace(node);
+        
+        while (!list.empty())
+        {
+            Node* cur = list.front();
+            list.pop();
+            // 打印当前结点
+            cout << cur->val << endl;
+            // 查找与当前点相连的所有点
+            for (auto n : cur->nexts)
+            {
+                // 如果n没有访问过则加入队列，否则跳过
+                if (visited.find(n) == visited.end())
+                {
+                    list.emplace(n);
+                    visited.emplace(n);
+                }
+            }
+        }
+    }
+    // 深度度优先遍历
+    void DFS(Node* node)
+    {
+        if (!node)
+        {
+            return;
+        }
+        stack<Node*> list;
+        unordered_set<Node*> visited;
+        list.emplace(node);
+        visited.emplace(node);
+        // 结点入栈时打印
+        cout << node->val << endl;
+        while (!list.empty())
+        {
+            Node* cur = list.top();
+            list.pop();
+            // 查找与当前点相连的所有点
+            for (auto n : cur->nexts)
+            {
+                // 如果没有访问过就将当前点，和相连的点先后入栈，便于回溯
+                if (visited.find(n) == visited.end())
+                {
+                    list.emplace(cur);
+                    list.emplace(n);
+                    visited.emplace(n);
+                    // 打印相连点
+                    cout << n->val << endl;
+                    // 结束遍历
+                    break;
+                }
+            }
+        }
+    }
+    // 拓扑排序，有向图中按拓扑（依赖关系）输出所有的点
+    vector<Node*> sortTopology(Graph* graph)
+    {
+        // 存储结果
+        vector<Node*> ans;
+        
+        // 将图中的所有点作为key,入度作为value存入hashmap
+        unordered_map<Node*, int> hashmap;
+        queue<Node*> zeroInQueue; // 入度为0的点才能入队列
+        
+        // 遍历图中的所有点
+        for (auto [_1, node] : graph->nodes)
+        {
+            // 找到入度为0的点入队
+            if (!node->in)
+            {
+                zeroInQueue.emplace(node);
+            }
+            else
+            {
+                hashmap.emplace(node, node->in); // 将入度不为0的点放入hashmap
+            }
+        }
+        
+        // 依次弹出队列中的点
+        while (!zeroInQueue.empty())
+        {
+            Node* curnode = zeroInQueue.front();
+            zeroInQueue.pop();
+            // 保存弹出的点的信息
+            ans.emplace_back(curnode);
+            // 遍历所有与当前点相关的点
+            for (auto node : curnode->nexts)
+            {
+                // 所有遍历到的点的入度--
+                hashmap[node]--;
+                // 如果入度为0入队
+                if (hashmap[node] == 0)
+                {
+                    zeroInQueue.emplace(node);
+                }
+            }
+        }
+        return ans;
+    }
+    // 最小生成树，kruskal算法,适用于无向图
+    vector<Edge*> kruskalMST(Graph* graph)
+    {
+        vector<Edge*> ans;
+        // 使用优先级队列，自定义比较函数，将所有边按权值大小放入优先级队列中，每次取权值最小的边
+        priority_queue<Edge*, vector<Edge*>, mycmp> edgequeue;
+        // 遍历所有边，加入小顶堆
+        for (auto edge : graph->edges)
+        {
+            edgequeue.push(edge);
+        }
+        // 构建点的并查集,将所有点创建一个集合
+        UnionFindeSet* uset = new UnionFindeSet(graph);
+        // 从小顶堆中遍历所有边
+        while (!edgequeue.empty())
+        {
+            Edge* curedge = edgequeue.top();
+            edgequeue.pop();
+            // 找到当前边对应的两点
+            Node* from = curedge->from;
+            Node* to = curedge->to;
+            // 判断两点是否是一个集合
+            if (!uset->isSameSet(from, to))
+            {
+                // 合并两点所在的集合
+                uset->unionSet(from, to);
+                // 记录当前边
+                ans.emplace_back(curedge);
+            }
+        }
+
+        return ans;
+    }
+private:
+    // 并查集结构
+    class UnionFindeSet
+    {
+    public:
+        unordered_map<Node*, unordered_set<Node*>*> nodesetmap; // 记录每一个点所在的集合
+        vector<unordered_set<Node*>*> sets; // 存储所有的点集合，用于析构
+        UnionFindeSet(Graph* graph) // 构造，将所有点放到各自的集合中
+        {
+            // 遍历图中所有的点
+            for (auto [_1, node] : graph->nodes)
+            {
+                // 每个点创建一个新集合，并相互对应
+                unordered_set<Node*>* newset = new unordered_set<Node*>;
+                // 将创建的set与node一一绑定
+                nodesetmap.emplace(node, newset);
+                // 将创建的新set放入sets，便于析构
+                sets.emplace_back(newset);
+            }
+        }
+        ~UnionFindeSet()
+        {
+            for (auto newset : this->sets)
+            {
+                if(newset) delete newset;
+            }
+        }
+    public:
+        // 查询点是否在一个集合中
+        bool isSameSet(Node* from, Node* to)
+        {
+            // 在map中分别找到两个点的set地址
+            unordered_set<Node*>* fromset = this->nodesetmap[from];
+            unordered_set<Node*>* toset = this->nodesetmap[to];
+            return fromset == toset;
+        }
+        // 合并两个所在的set
+        void unionSet(Node* from, Node* to)
+        {
+            // 在map中分别找到两个点的set地址
+            unordered_set<Node*>* fromset = this->nodesetmap[from];
+            this->nodesetmap[to] = fromset;
+        }
+    };
+
+    // 边的小顶堆
+    struct mycmp
+    {
+        bool operator()(Edge* edge1, Edge* edge2)
+        {
+            return edge1->weight > edge2->weight;
+        }    
+    };
+};
+
 
 void inputData(int n, vector<vector<int>>& matrix)
 {
@@ -150,7 +363,18 @@ int main()
     inputData(3, matrix);
     // outputData(matrix);
     Graph* graph = vectortoGraph(matrix);
-    // if (graph) delete graph;
+    // 宽度优先遍历
+    // graph->BFS(graph->nodes[0]);
+    // 深度优先遍历
+    // graph->DFS(graph->nodes[0]);
+    vector<Edge*> ans = graph->kruskalMST(graph);
+    for (auto edge : ans)
+    {
+        cout << edge->weight << endl;
+    }
+    
+    
+    if (graph) delete graph;
     
     return 0;
 }
